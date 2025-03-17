ea0a83f57acb0dfcc19c3768e37e8170f863ef62
MoonDongmin <20191836@o365.hanbat.ac.kr>
2025-03-17 18:03:57 +0900

    typeorm 커넥션 맺기

--- chapter03/src/app.module.ts
+++ chapter03/src/app.module.ts
@@ -14,6 +14,9 @@ import {UsersModule}      from "@/users/users.module";
 import {ChannelsModule}   from "@/channels/channels.module";
 import {DmsModule}        from "@/dms/dms.module";
 import {WorkspacesModule} from "@/workspaces/workspaces.module";
+import {TypeOrmModule}    from "@nestjs/typeorm";
+import * as process       from "node:process";
+import {Users}            from "@/entities/Users";
 
 @Module({
     imports: [
@@ -24,6 +27,22 @@ import {WorkspacesModule} from "@/workspaces/workspaces.module";
         ChannelsModule,
         DmsModule,
         WorkspacesModule,
+        TypeOrmModule.forRoot({
+            type: "mysql",
+            host: "localhost",
+            port: 3306,
+            username: process.env.DB_USERNAME,
+            password: process.env.DB_PASSWORD,
+            database: process.env.DB_DATABASE,
+            // entities: ["entities/*"],
+            autoLoadEntities: true,
+            // 우리 DB에서 이걸 entities를 가져올 때 이 속성을 추가
+            // DB에 entities를 넣고 나면 다시 false로 바꿈
+            synchronize: true,
+            logging: true,
+            charset: "utf8mb4",
+        }),
+        TypeOrmModule.forFeature([Users]),
     ],
     controllers: [
         AppController,
