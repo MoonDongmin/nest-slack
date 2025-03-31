b00227aceb6cfe12a1f8f66256d504158a2f82bb
MoonDongmin <20191836@o365.hanbat.ac.kr>
2025-03-31 17:02:33 +0900

    typeorm 쿼리빌더

--- chapter04/src/workspaces/workspaces.service.ts
+++ chapter04/src/workspaces/workspaces.service.ts
@@ -41,5 +41,36 @@ export class WorkspacesService {
     }
 
     async createWorkspace(name: string, url: string, myId: number) {
+        const workspace = this.workspacesRepository.create({
+            name,
+            url,
+            OwnerId: myId,
+        });
+        const returned = await this.workspacesRepository.save(workspace);
+
+        const workspaceMember = new WorkspaceMembers();
+        workspaceMember.UserId = myId;
+        workspaceMember.WorkspaceId = returned.id;
+        await this.workspaceMembersRepository.save(workspaceMember);
+
+        const channel = new Channels();
+        channel.name = "일반";
+        channel.WorkspaceId = returned.id;
+
+        const channelReturned = await this.channelsRepository.save(channel);
+
+        const channelMember = new ChannelMembers();
+        channelMember.UserId = myId;
+        channelMember.ChannelId = channelReturned.id;
+        await this.channelMembersRepository.save(channelMember);
+    }
+
+    // QueryBuilder
+    async getWorkspaceMembers(url: string) {
+        return this.usersRepository
+            .createQueryBuilder("user")
+            .innerJoin("user.Workspace", "workspace")
+            .innerJoin("members.Workspace", "workspace", "workspace.url = :url", {url: url})
+            .getMany();
     }
 }
