1a3c2b05a3c68e572554d44a9126de48ec7d3ed6
MoonDongmin <20191836@o365.hanbat.ac.kr>
2025-03-31 16:24:04 +0900

    typeorm transaction

--- chapter04/src/users/users.service.ts
+++ chapter04/src/users/users.service.ts
@@ -1,33 +1,82 @@
 import {
-    BadRequestException,
-    HttpException,
     Injectable,
     UnauthorizedException,
-} from "@nestjs/common";
+}                         from "@nestjs/common";
 import {InjectRepository} from "@nestjs/typeorm";
 import {Users}            from "@/entities/Users";
-import {Repository}       from "typeorm";
+import {
+    DataSource,
+    Repository,
+}                         from "typeorm";
 import bcrypt             from "bcrypt";
+import {WorkspaceMembers} from "@/entities/WorkspaceMembers";
+import {ChannelMembers}   from "@/entities/ChannelMembers";
 
 @Injectable()
 export class UsersService {
     constructor(
         @InjectRepository(Users)
         private usersRepository: Repository<Users>,
+        @InjectRepository(WorkspaceMembers)
+        private workspaceMembersRepository: Repository<WorkspaceMembers>,
+        @InjectRepository(ChannelMembers)
+        private channelMembersRepository: Repository<ChannelMembers>,
+        private dataSource: DataSource,
     ) {
     }
 
     async join(email: string, nickname: string, password: string) {
-        const user = await this.usersRepository.findOne({where: {email}});
-        if (user) {
-            throw new UnauthorizedException("이미 존재하는 사용자 입니다.");
-        }
+        const queryRunner = this.dataSource.createQueryRunner();
+        await queryRunner.connect();
+        await queryRunner.startTransaction();
 
+        // queryRunner로 이를 불러올 때는 manager.getRepository("")이런식으로 불러와야 함
         const hashedPassword = await bcrypt.hash(password, 12);
-        await this.usersRepository.save({
-            email,
-            nickname,
-            password: hashedPassword,
-        });
+
+        const user = await queryRunner.manager
+            .getRepository(Users)
+            .findOne({where: {email}});
+
+
+        try {
+            if (user) {
+                throw new UnauthorizedException("이미 존재하는 사용자 입니다.");
+            }
+
+            const returned = await queryRunner.manager
+                .getRepository(Users)
+                .save({
+                    email,
+                    nickname,
+                    password: hashedPassword,
+                });
+
+
+            const workspaceMember = await queryRunner.manager
+                .getRepository(WorkspaceMembers)
+                .create();
+            workspaceMember.UserId = returned.id;
+            workspaceMember.WorkspaceId = 1;
+
+            await queryRunner.manager
+                .getRepository(WorkspaceMembers)
+                .save(workspaceMember);
+
+            await queryRunner.manager
+                .getRepository(ChannelMembers)
+                .save({
+                    UserId: returned.id,
+                    ChannelId: 1,
+                });
+
+            await queryRunner.commitTransaction();
+            return true;
+        } catch (error) {
+            console.error(error);
+            await queryRunner.rollbackTransaction();
+            throw error;
+        } finally {
+            await queryRunner.release();
+        }
     }
 }
