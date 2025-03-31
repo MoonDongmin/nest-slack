1dcb358ccd679edf5f3da68ca3c9cc0c486856f4
MoonDongmin <20191836@o365.hanbat.ac.kr>
2025-03-31 16:44:59 +0900

    ParseIntPipe, ParseArrayPipe, PickType

--- chapter04/src/workspaces/workspaces.controller.ts
+++ chapter04/src/workspaces/workspaces.controller.ts
@@ -1,21 +1,35 @@
 import {
+    Body,
     Controller,
     Delete,
     Get,
+    Param,
+    ParseIntPipe,
     Post,
-} from "@nestjs/common";
-import {ApiTags} from "@nestjs/swagger";
+}                           from "@nestjs/common";
+import {ApiTags}            from "@nestjs/swagger";
+import {WorkspacesService}  from "@/workspaces/workspaces.service";
+import {User}               from "@/common/decorators/user.decorator";
+import {Users}              from "@/entities/Users";
+import {CreateWorkspaceDto} from "@/workspaces/dto/create-workspace.dto";
 
 @ApiTags("WORKSPACE")
 @Controller("api/workspaces")
 export class WorkspacesController {
-    @Get()
-    getMyWorkspaces() {
+    constructor(
+        private workspacesService: WorkspacesService,
+    ) {
+    }
 
+    @Get("/:myId")
+    // ParseIntPipe를 사용해서 string값을 알아서 number로 바꿔줌
+    getMyWorkspaces(@User() user: Users) {
+        return this.workspacesService.findMyWorkspaces(user.id);
     }
 
     @Post()
-    createWorkspace() {
+    createWorkspace(@User() user: Users, @Body() body: CreateWorkspaceDto) {
+        return this.workspacesService.createWorkspace(body.workspace, body.url, user.id);
     }
 
     @Get(":url/members")
--- chapter04/src/workspaces/workspaces.service.ts
+++ chapter04/src/workspaces/workspaces.service.ts
@@ -1,4 +1,45 @@
-import { Injectable } from '@nestjs/common';
+import {Injectable}       from "@nestjs/common";
+import {InjectRepository} from "@nestjs/typeorm";
+import {Users}            from "@/entities/Users";
+import {
+    DataSource,
+    Repository,
+}                         from "typeorm";
+import {WorkspaceMembers} from "@/entities/WorkspaceMembers";
+import {ChannelMembers}   from "@/entities/ChannelMembers";
+import {Workspaces}       from "@/entities/Workspaces";
+import {Channels}         from "@/entities/Channels";
 
 @Injectable()
-export class WorkspacesService {}
+export class WorkspacesService {
+    constructor(
+        @InjectRepository(Workspaces)
+        private workspacesRepository: Repository<Workspaces>,
+        @InjectRepository(Channels)
+        private channelsRepository: Repository<Channels>,
+        @InjectRepository(WorkspaceMembers)
+        private workspaceMembersRepository: Repository<WorkspaceMembers>,
+        @InjectRepository(ChannelMembers)
+        private channelMembersRepository: Repository<ChannelMembers>,
+        @InjectRepository(Users)
+        private usersRepository: Repository<Users>,
+    ) {
+    }
+
+    async findById(id: number) {
+        return this.workspacesRepository.findOne({
+            where: {id},
+        });
+    }
+
+    async findMyWorkspaces(myId: number) {
+        return this.workspacesRepository.find({
+            where: {
+                WorkspaceMembers: [{UserId: myId}],
+            },
+        });
+    }
+
+    async createWorkspace(name: string, url: string, myId: number) {
+    }
+}
